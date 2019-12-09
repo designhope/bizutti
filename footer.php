395db4a21346e4f5528a50d053f24da3becc768a
Rogerio <rogerio@designhope.com.br>
2019-12-08 21:32:45 -0300

    first commit

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 </footer>
 </main>
-<script src="https://maps.googleapis.com/maps/api/js?&amp;extension=.js&amp;key=AIzaSyCxHp9Ix1vVHN4VZCP0JZpYEnYGffsoQSw"></script>
+<script src="https://maps.googleapis.com/maps/api/js?&amp;extension=.js&amp;key=AIzaSyAC5IfB1yZKTOpBcpo80WcfjEwpS5sEW1Q"></script>
 <script src="assets/js/script.js"></script>
 </body>
 
